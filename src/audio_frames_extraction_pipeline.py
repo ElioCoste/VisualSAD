b81119b50dcacbc8e8eec23327611852c90b5476
celdot <celine.dothuan@telecom-paris.fr>
2025-05-12 19:02:58 +0200

    modified talknet preprocessing to process only a subset of the videos

--- src/audio_frames_extraction_pipeline.py
+++ src/audio_frames_extraction_pipeline.py
@@ -1,3 +1,4 @@
+import argparse
 import glob
 import os
 import subprocess
@@ -12,23 +13,26 @@ from tqdm import tqdm
 
 class Extractor:
     def __init__(self):
-        self.DATASET_DIR = os.path.join(Path.cwd().parent, "data")
-        self.VIDEO_DIR = os.path.join(self.DATASET_DIR, "orig_videos")
-        self.AUDIO_DIR = os.path.join(self.DATASET_DIR, "orig_audios")
-        self.FRAMES_DIR = os.path.join(self.DATASET_DIR, "frames")
-        self.VIDEO_CLIPS_DIR = os.path.join(self.DATASET_DIR, "clips_videos")
-        self.AUDIO_CLIPS_DIR = os.path.join(self.DATASET_DIR, "clips_audios")
-        os.makedirs(self.AUDIO_DIR, exist_ok=True)
-        os.makedirs(self.FRAMES_DIR, exist_ok=True)
-        os.makedirs(self.AUDIO_CLIPS_DIR, exist_ok=True)
-        os.makedirs(self.VIDEO_CLIPS_DIR, exist_ok=True)
+        self.dataset_dir = os.path.join(Path.cwd().parent, "data")
+        self.video_dir = os.path.join(self.dataset_dir, "orig_videos")
+        self.audio_dir = os.path.join(self.dataset_dir, "orig_audios")
+        self.frames_dir = os.path.join(self.dataset_dir, "frames")
+        self.video_clips_dir = os.path.join(self.dataset_dir, "clips_videos")
+        self.audio_clips_dir = os.path.join(self.dataset_dir, "clips_audios")
+        self.annotations_dir = os.path.join(self.dataset_dir, "csv")
+        os.makedirs(self.audio_dir, exist_ok=True)
+        os.makedirs(self.frames_dir, exist_ok=True)
+        os.makedirs(self.audio_clips_dir, exist_ok=True)
+        os.makedirs(self.video_clips_dir, exist_ok=True)
         
-        self.modes = ["train", "val"]
+        self.modes = ["train", "val", "test"]
         for m in self.modes:
-            os.makedirs(os.path.join(self.AUDIO_DIR, m), exist_ok=True)
-            os.makedirs(os.path.join(self.FRAMES_DIR, m), exist_ok=True)
-            os.makedirs(os.path.join(self.AUDIO_CLIPS_DIR, m), exist_ok=True)
-            os.makedirs(os.path.join(self.VIDEO_CLIPS_DIR, m), exist_ok=True)
+            os.makedirs(os.path.join(self.audio_dir, m), exist_ok=True)
+            os.makedirs(os.path.join(self.frames_dir, m), exist_ok=True)
+            os.makedirs(os.path.join(self.audio_clips_dir, m), exist_ok=True)
+            os.makedirs(os.path.join(self.video_clips_dir, m), exist_ok=True)
+        
+        self.files_names = dict.fromkeys(self.modes)
 
     def extract_audio(self, input_video, output_audio):
         """
@@ -44,11 +48,17 @@ class Extractor:
         command = (f"ffmpeg -i {input_video} -vf fps=20 -q:v 2 {output_frames}")
         subprocess.call(command, shell=True, stdout=None)
         
-    def create_annotations_df(self, cvs_file):
+    def create_annotations_df(self, mode, use_subset=True):
         """
         Create a dataframe from the csv file
         """
-        df = pd.read_csv(cvs_file)
+        df = pd.read_csv(os.path.join(self.annotations_dir, f"{mode}_orig.csv"))
+        if use_subset:
+            with open(os.path.join(self.annotations_dir, f"{mode}_subset_file_list.txt"), "r") as f:
+                self.file_names[mode] = list(map(lambda line: line.strip(), f.readlines()))
+            # Drop the rows where video id is not in the subset
+            df = df[df['video_id'].isin(self.file_names[mode])]
+        
         df_neg = pd.concat([df[df['label_id'] == 0], df[df['label_id'] == 2]])
         df_pos = df[df['label_id'] == 1]
         df = pd.concat([df_pos, df_neg]).reset_index(drop=True)
@@ -58,76 +68,85 @@ class Extractor:
         
         return df, entity_list
         
-    def extract_audio_clips(self, entity_list, df, output_dir, input_dir):
+    def extract_audio_clips(self, entity, df, mode, output_dir, input_dir):
         """
         Extract audio clips for each entity from the video
         """
         audio_features = {}
-        for entity in tqdm(entity_list):
-            ins_data = df.get_group(entity)
-            video_key = ins_data.iloc[0]['video_id']
-            start = ins_data.iloc[0]['frame_timestamp']
-            end = ins_data.iloc[-1]['frame_timestamp']
-            entity_id = ins_data.iloc[0]['entity_id']
-            ins_path = os.path.join(output_dir, video_key, entity_id+'.wav')
-            if video_key not in audio_features.keys():
-                audio_file = os.path.join(input_dir, video_key+'.wav')
-                sr, audio = wavfile.read(audio_file)
-                audio_features[video_key] = audio
-            audio_start = int(float(start)*sr)
-            audio_end = int(float(end)*sr)
-            audio_data = audio_features[video_key][audio_start:audio_end]
-            wavfile.write(ins_path, sr, audio_data)
+        ins_data = df.get_group(entity)
+        video_key = ins_data.iloc[0]['video_id']
+        start = ins_data.iloc[0]['frame_timestamp']
+        end = ins_data.iloc[-1]['frame_timestamp']
+        entity_id = ins_data.iloc[0]['entity_id']
+        ins_path = os.path.join(output_dir, mode, video_key, entity_id+'.wav')
+        if video_key not in audio_features.keys():
+            audio_file = os.path.join(input_dir, mode, video_key+'.wav')
+            sr, audio = wavfile.read(audio_file)
+            audio_features[video_key] = audio
+        audio_start = int(float(start)*sr)
+        audio_end = int(float(end)*sr)
+        audio_data = audio_features[video_key][audio_start:audio_end]
+        wavfile.write(ins_path, sr, audio_data)
             
-    def extract_video_clips(self, entity_list, df, output_dir, input_dir):
+    def extract_video_clips(self, entity, df, mode, output_dir, input_dir):
         """
         Extract video clips for each entity from the video
         """
-        for entity in tqdm(entity_list):
-            ins_data = df.get_group(entity)
-            video_key = ins_data.iloc[0]['video_id']
-            video_file = glob.glob(os.path.join(input_dir, '{}.*'.format(video_key)))[0]
-            V = cv2.VideoCapture(video_file)
-            j = 0
-            for _, row in ins_data.iterrows():
-                image_filename = os.path.join(output_dir, str("%.2f"%row['frame_timestamp'])+'.jpg')
-                V.set(cv2.CAP_PROP_POS_MSEC, row['frame_timestamp'] * 1e3)
-                _, frame = V.read()
-                h = np.size(frame, 0)
-                w = np.size(frame, 1)
-                x1 = int(row['entity_box_x1'] * w)
-                y1 = int(row['entity_box_y1'] * h)
-                x2 = int(row['entity_box_x2'] * w)
-                y2 = int(row['entity_box_y2'] * h)
-                face = frame[y1:y2, x1:x2, :]
-                j = j+1
-                cv2.imwrite(image_filename, face)
+        ins_data = df.get_group(entity)
+        video_key = ins_data.iloc[0]['video_id']
+        video_file = glob.glob(os.path.join(input_dir, mode, '{}.*'.format(video_key)))[0]
+        V = cv2.VideoCapture(video_file)
+        j = 0
+        for _, row in ins_data.iterrows():
+            image_filename = os.path.join(output_dir, mode, str("%.2f"%row['frame_timestamp'])+'.jpg')
+            V.set(cv2.CAP_PROP_POS_MSEC, row['frame_timestamp'] * 1e3)
+            _, frame = V.read()
+            h = np.size(frame, 0)
+            w = np.size(frame, 1)
+            x1 = int(row['entity_box_x1'] * w)
+            y1 = int(row['entity_box_y1'] * h)
+            x2 = int(row['entity_box_x2'] * w)
+            y2 = int(row['entity_box_y2'] * h)
+            face = frame[y1:y2, x1:x2, :]
+            j = j+1
+            cv2.imwrite(image_filename, face)
         
-def main():
+def main(use_subset):
     extractor = Extractor()
     
     for m in extractor.modes:
-        video_dir = os.path.join(extractor.VIDEO_DIR, m)
-        video_names = os.listdir(video_dir)[:5]
-        print(video_names)
+        video_dir = os.path.join(extractor.video_dir, m)
+        video_names = os.listdir(video_dir)
         for video in tqdm(video_names):
             input_video = os.path.join(video_dir, video)
             video_id = video.split(".")[0]
             
-            output_audio = os.path.join(extractor.AUDIO_DIR, m, video_id + ".wav")
+            output_audio = os.path.join(extractor.audio_dir, m, video_id + ".wav")
             
             if os.path.exists(output_audio):
                 print(f"Audio file {output_audio} already exists. Skipping extraction.")
             else:
                 extractor.extract_audio(input_video, output_audio)
                 
-            frames_dir = os.path.join(extractor.FRAMES_DIR, m, video_id)
+            frames_dir = os.path.join(extractor.frames_dir, m, video_id)
             os.makedirs(frames_dir, exist_ok=True)
             output_frames = os.path.join(frames_dir, "img_%06d.jpg")
             if os.path.exists(frames_dir) and len(os.listdir(frames_dir)) > 0:
                 print(f"Frames for {video} already exist. Skipping extraction.")
             else: 
                 extractor.extract_frames(input_video, output_frames)
+        
+        df, entity_list = extractor.create_annotations_df(m, use_subset)
+        for entity in tqdm(entity_list):
+            extractor.extract_audio_clips(entity, df, m, extractor.audio_clips_dir, extractor.audio_dir)
+            extractor.extract_video_clips(entity, df, m, extractor.video_clips_dir, extractor.video_dir)
             
 if __name__ == '__main__':
-    main()
+    argparse = argparse.ArgumentParser(description="Extract audio and frames from videos")
+    argparse.add_argument('--use_subset', action='store_true', help="Use subset of the dataset")
+    
+    args = argparse.parse_args()
+    
+    use_subset = args.use_subset
+    
+    main(use_subset)
--- src/download_data_pipeline.py
+++ src/download_data_pipeline.py
@@ -1,112 +1,163 @@
+import argparse
 import os
-import subprocess
+import random
 from pathlib import Path
 
+import pandas as pd
 import requests
 from tqdm import tqdm
 
 
 class AVADataset:
     def __init__(self):
-        self.DATASET_DIR = os.path.join(Path.cwd().parent, "data")
-        self.VIDEO_DIR = os.path.join(self.DATASET_DIR, "orig_videos")
-        os.makedirs(self.VIDEO_DIR, exist_ok=True)
-
-        # Create the train and validation directories if they don't exist
-        self.TRAIN_DIR = os.path.join(self.VIDEO_DIR, "train")
-        self.VAL_DIR = os.path.join(self.VIDEO_DIR, "val")
-        os.makedirs(self.TRAIN_DIR, exist_ok=True)
-        os.makedirs(self.VAL_DIR, exist_ok=True)
+        self.dataset_dir = os.path.join(Path.cwd().parent, "data")
+        self.video_dir = os.path.join(self.dataset_dir, "orig_videos")
+        os.makedirs(self.video_dir, exist_ok=True)
         
-        self.ANNOTATIONS_DIR = os.path.join(self.DATASET_DIR, "csv")
-        self.ANNOTATIONS_TRAIN = os.path.join(self.ANNOTATIONS_DIR, "train")
-        self.ANNOTATIONS_VAL = os.path.join(self.ANNOTATIONS_DIR, "val")
+        self.modes = ["train", "val", "test"]
+        self.conversion_mode = {"train": "trainval", "val": "trainval", "test": "test"}
 
-        self.TRAIN_IDS = []
-        self.VAL_IDS = []
-        self.FILE_NAMES = []
-        self.FILE_IDS = []
+        # Create the train, validation and test directories if they don't exist
+        for mode in self.modes:
+            os.makedirs(os.path.join(self.video_dir, mode), exist_ok=True)
+        
+        self.annotations_dir = os.path.join(self.dataset_dir, "csv")
 
-    def get_trainval_ids(self):
+        self.video_ids = dict.fromkeys(self.modes)
+        self.file_names = dict.fromkeys(self.modes)
+        self.file_ids = dict.fromkeys(self.modes)
+        
+    def get_subset(self, subset_nb, subset_exists=False):
+        """
+        Get the subset of the AVA dataset
+        """
+        if subset_exists:
+            self.file_names = dict.fromkeys(self.modes)
+            for mode in self.modes:
+                with open(os.path.join(self.annotations_dir, f"{mode}_subset_file_list.txt"), "r") as f:
+                    self.file_names[mode] = list(map(lambda line: line.strip(), f.readlines()))
+                    self.file_ids[mode] = list(map(lambda line: line.split(".")[0], self.file_names[mode]))
+        else:
+            random.seed(42)
+            subset_names_list = dict.fromkeys(self.modes)
+            for mode in self.modes:
+                subset_names_list[mode] = random.choices(self.file_names[mode], sample=subset_nb[mode])
+                self.file_names[mode] = subset_names_list[mode]
+                self.file_ids[mode] = list(map(lambda line: line.split(".")[0], subset_names_list[mode]))
+                # Write the subset to a text file
+                with open(os.path.join(self.annotations_dir, f"{mode}_subset_file_list.txt"), "w") as f:
+                    for name in subset_names_list[mode]:
+                        f.write(name + "\n")
+
+    def get_video_ids(self):
         """
         Get the training and validation IDs from the AVA dataset
         """
-        self.TRAIN_IDS = os.listdir(self.ANNOTATIONS_TRAIN)
-        self.VAL_IDS = os.listdir(self.ANNOTATIONS_VAL)
-        self.TRAIN_IDS = list(map(lambda x: x.split("-activespeaker")[0], self.TRAIN_IDS))
-        self.VAL_IDS = list(map(lambda x: x.split("-activespeaker")[0], self.VAL_IDS))
-        print(f"Number of training files: {len(self.TRAIN_IDS)}")
-        print(f"Number of validation files: {len(self.VAL_IDS)}")
+        df_dict = dict.fromkeys(self.modes)
+        for mode in self.modes:
+            df_dict[mode] = pd.read_csv(os.path.join(self.annotations_dir, f"{mode}_orig.csv"))
+            self.video_ids[mode] = df_dict[mode]['video_id'].unique().tolist()
 
-    def read_file_ids(self, path="ava_speech_file_names_v1.txt"):
+    def read_file_ids(self, mode):
         """
         Read the file IDs from the text file
         """
-        with open(os.path.join(self.DATASET_DIR, path), "r") as f:
-            self.FILE_NAMES = list(map(lambda line: line.strip(), f.readlines()))
-            self.FILE_IDS = list(map(lambda line: line.split(".")[0], self.FILE_NAMES))
+        with open(os.path.join(self.annotations_dir, f'{mode}_file_list.txt'), "r") as f:
+            if mode == "trainval":
+                trainval_ids = list(map(lambda line: line.split(".")[0], self.file_names))
+                trainval_names = list(map(lambda line: line.strip(), f.readlines()))
+                for i in range(len(trainval_ids)):
+                    if trainval_ids[i] in self.video_ids["train"]:
+                        self.file_ids["train"].append(trainval_ids[i])
+                        self.file_names["train"].append(trainval_names[i])
+                    elif trainval_ids[i] in self.video_ids["val"]:
+                        self.file_ids["val"].append(trainval_ids[i])
+                        self.file_names["val"].append(trainval_names[i])
+            else:
+                self.file_ids[mode] = list(map(lambda line: line.split(".")[0], f.readlines()))
+                self.file_names[mode] = list(map(lambda line: line.strip(), f.readlines()))
             
     def get_dataset_statistics(self):
         """
         Check if the file IDs are in the training and validation lists
         """
-        nb_file_not_found = 0
-        for file_id in self.FILE_IDS:
-            if file_id not in self.TRAIN_IDS and file_id not in self.VAL_IDS:
-                print(f"File ID {file_id} not found in training or validation list.")
-                nb_file_not_found += 1
+        # Check if the training, test and validation lists are in the csv IDs
+        nb_file_not_found_in_csv = dict.fromkeys(self.modes, 0)
+        for mode in self.modes:
+            for file_id in self.file_ids[mode]:
+                if file_id not in self.video_ids[mode]:
+                    print(f"File ID {file_id} not found in {mode} list.")
+                    nb_file_not_found_in_csv[mode] += 1
 
-        # Check if the training and validation lists are in the file IDs
-        nb_train_files_not_found = 0
-        nb_val_file_not_found = 0
-        for file_id in self.TRAIN_IDS:
-            if file_id not in self.FILE_IDS:
-                print(f"File ID {file_id} not found in file IDs list.")
-                nb_train_files_not_found += 1
-        for file_id in self.VAL_IDS:
-            if file_id not in self.FILE_IDS:
-                print(f"File ID {file_id} not found in file IDs list.")
-                nb_val_file_not_found += 1
-                
-        print(f"Number of file IDs not found: {nb_file_not_found}")
-        print(f"Number of training files not found: {nb_train_files_not_found}")
-        print(f"Number of validation files not found: {nb_val_file_not_found}")
+        # Check if the CSV IDs are in the training, test and validation lists
+        nb_csv_id_not_found = dict.fromkeys(self.modes, 0)
+        for mode in self.modes:
+            for file_id in self.video_ids[mode]:
+                if file_id not in self.file_ids[mode]:
+                    print(f"File ID {file_id} not found in {mode} file IDs list.")
+                    nb_csv_id_not_found[mode] += 1
+                    
+        print(f"Number of file IDs not found in CSV: {nb_file_not_found_in_csv}")
+        print(f"Number of CSV IDs not found in file IDs: {nb_csv_id_not_found}")
         
-    def download_file(self, filename):
+    def download_file(self, filename, mode):
         """
         Download the file from the AVA dataset with the given filename
         """
-        url = f'https://s3.amazonaws.com/ava-dataset/trainval/{filename}'
-        if os.path.isfile(os.path.join(self.TRAIN_DIR, filename)) or os.path.isfile(os.path.join(self.VAL_DIR, filename)):
+        url = f'https://s3.amazonaws.com/ava-dataset/{mode}/{filename}'
+        if os.path.isfile(os.path.join(self.video_dir, mode, filename)):
             print(f"File {filename} already exists. Skipping.")
         else:
             r = requests.get(url, allow_redirects=True)
-            if filename.split(".")[0] in self.TRAIN_IDS:
-                open(os.path.join(self.TRAIN_DIR, filename), 'wb').write(r.content)
-            elif filename.split(".")[0] in self.VAL_IDS:
-                open(os.path.join(self.VAL_DIR, filename), 'wb').write(r.content)
+            if filename in self.file_names[self.conversion_mode[mode]]:
+                open(os.path.join(self.video_dir, self.conversion_mode[mode], filename), 'wb').write(r.content)
             else:
-                print(f"File ID {filename.split('.')[0]} not found in training or validation list.")
+                print(f"File ID {filename.split('.')[0]} not found in list.")
             
     def download_all_files(self):
         """
         Download all files from the AVA dataset
         """
-        for filename in tqdm(self.FILE_NAMES):
-            self.download_file(filename)
+        for mode in self.modes:
             
+            for filename in tqdm(self.file_names[mode]):
+                self.download_file(filename, mode)
 
-
-def main():
+def main(use_subset=True, subset_exists=False, train_subset=20, val_subset=10, test_subset=5):
     """
     Main function to run the data pipeline
     """
+    subset_nb = {"train": train_subset, "val": val_subset, "test": test_subset}
+    
     dataset = AVADataset()
-    # dataset.get_trainval_ids()
-    # dataset.read_file_ids()
+    dataset.get_video_ids()
+    if use_subset and not subset_exists:
+        dataset.read_file_ids("trainval")
+        dataset.read_file_ids("test")
+        dataset.get_subset(subset_nb=subset_nb, subset_exists=subset_exists)
+    if use_subset and subset_exists:
+        dataset.get_subset(subset_nb=None, subset_exists=subset_exists)
+    if not use_subset:
+        dataset.read_file_ids("trainval")
+        dataset.read_file_ids("test")
         
-    # dataset.download_all_files()
-    dataset.download_csv(os.path.join(dataset.DATASET_DIR, "csv_new"))
+    dataset.get_dataset_statistics()
+    dataset.download_all_files()
 
 if __name__ == "__main__":
-    main()
+    argparse = argparse.ArgumentParser(description="Download AVA dataset")
+    argparse.add_argument("--use_subset", type=bool, default=True, help="Use subset of the dataset")
+    argparse.add_argument("--subset_exists", type=bool, default=False, help="Use existing subset of the dataset")
+    argparse.add_argument("--train_subset", type=int, default=20, help="Number of training samples")
+    argparse.add_argument("--val_subset", type=int, default=10, help="Number of validation samples")
+    argparse.add_argument("--test_subset", type=int, default=5, help="Number of test samples")
+    
+    args = argparse.parse_args()
+    
+    use_subset = args.use_subset
+    subset_exists = args.subset_exists
+    train_subset = args.train_subset
+    val_subset = args.val_subset
+    test_subset = args.test_subset
+    
+    main(use_subset=use_subset, subset_exists=subset_exists, train_subset=train_subset, val_subset=val_subset, test_subset=test_subset)
