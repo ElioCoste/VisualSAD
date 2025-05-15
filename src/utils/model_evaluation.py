67a6a0226634e92f38ffd8c49698d6ba98a85d35
celdot <celine.dothuan@telecom-paris.fr>
2025-05-15 10:41:33 +0200

    add eda and model evaluation

---
+++ src/utils/model_evaluation.py
@@ -0,0 +1,79 @@
+from pprint import PrettyPrinter
+
+import torch
+from tqdm import tqdm
+
+# Good formatting when printing the APs for each class and mAP
+pp = PrettyPrinter()
+
+# Parameters
+data_folder = './'
+keep_difficult = True  # difficult ground truth objects must always be considered in mAP calculation, because these objects DO exist!
+batch_size = 64
+workers = 4
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+checkpoint = './checkpoint_ssd300.pth.tar'
+
+# Load model checkpoint that is to be evaluated
+checkpoint = torch.load(checkpoint)
+model = checkpoint['model']
+model = model.to(device)
+
+# Switch to eval mode
+model.eval()
+
+# Load test data
+test_dataset = PascalVOCDataset(data_folder,
+                                split='test',
+                                keep_difficult=keep_difficult)
+data_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False,
+                                          num_workers=workers, pin_memory=True)
+
+
+def evaluate(data_loader, model):
+    """
+    Evaluate the model using mAP
+    
+    :param data_loader: DataLoader for the data
+    :param model: model
+    """
+
+    model.eval()
+
+    with torch.no_grad():
+        # Batches
+        for i, (images, boxes, labels, difficulties) in enumerate(tqdm(data_loader)):
+            images = images.to(device)  # (N, 3, 300, 300)
+
+            # Forward prop.
+            predicted_locs, predicted_scores = model(images)
+
+            # Detect objects in SSD output
+            det_boxes_batch, det_labels_batch, det_scores_batch = model.detect_objects(predicted_locs, predicted_scores,
+                                                                                       min_score=0.01, max_overlap=0.45,
+                                                                                       top_k=200)
+            # Evaluation MUST be at min_score=0.01, max_overlap=0.45, top_k=200 for fair comparision with the paper's results and other repos
+
+            # Store this batch's results for mAP calculation
+            boxes = [b.to(device) for b in boxes]
+            labels = [l.to(device) for l in labels]
+            difficulties = [d.to(device) for d in difficulties]
+
+            det_boxes.extend(det_boxes_batch)
+            det_labels.extend(det_labels_batch)
+            det_scores.extend(det_scores_batch)
+            true_boxes.extend(boxes)
+            true_labels.extend(labels)
+            true_difficulties.extend(difficulties)
+
+        # Calculate mAP
+        APs, mAP = calculate_mAP(det_boxes, det_labels, det_scores, true_boxes, true_labels, true_difficulties)
+
+    # Print AP for each class
+    pp.pprint(APs)
+
+    print('\nMean Average Precision (mAP): %.3f' % mAP)
+
+
+if __name__ == '__main__':
+    evaluate(data_loader, model)
